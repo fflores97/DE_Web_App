21bb69fa4c6891cba7723b7e9b71791b249046e6
Felipe Flores <felipeflores@college.harvard.edu>
2018-04-04 18:07:46 -0400

    Added PCA parameters to user

--- scripts/testScript.R
+++ scripts/testScript.R
@@ -6,3 +6,15 @@ sampleCorrelation <- print(sampleCorrelation(normalizedCountsTable = expressionM
 pdf(paste(sessionDir,"/correlationTest.pdf", sep = ""), width = 12, height=5)
 print(sampleCorrelation)
 dev.off()
+
+autoClustering <- FALSE
+clusters <- 3
+numberOfPCs <- 3
+
+
+pcaPlots <- samplePCA(
+  expressionMatrix,
+  numberOfPCs = numberOfPCs,
+  autoClustering = autoClustering,
+  clusters = clusters
+)
